d66f0c29c3d6f5a2920aad916cc4261f5e15504f
Therebel12138 <3300622462@qq.com>
2023-10-14 15:11:32 +0800

    Initial commit

---
+++ midware/jwt.go
@@ -0,0 +1,85 @@
+package midware
+
+import (
+	"errors"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	orm "liuliang/model"
+	"net/http"
+	"time"
+)
+
+var jwtkey = []byte("i_really_like_you")
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func Gettoken(username1 string) (string, error) {
+	expirationtime := time.Now().Add(1 * time.Hour)
+	claims := Claims{
+		username1,
+		jwt.StandardClaims{
+			ExpiresAt: expirationtime.Unix(),
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "zzw",
+			Subject:   "user token",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenstring, err := token.SignedString(jwtkey)
+	return tokenstring, err
+}
+
+func parsetoken(tokenstring string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenstring, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtkey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, errors.New("invalid token")
+}
+
+func Sendtoken() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		username := c.PostForm("username")
+		passwd := c.PostForm("passwd")
+		if orm.Userlogin(username, passwd) {
+			tokenstring, _ := Gettoken(username)
+			c.JSON(http.StatusOK, gin.H{
+				"token": tokenstring,
+			})
+			c.Next()
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "密码错误",
+			})
+			c.Abort()
+		}
+	}
+}
+
+func Twjparse() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		tokenstring := c.PostForm("token")
+		if tokenstring == "" {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "token为空",
+			})
+			c.Abort()
+		}
+		_, err := parsetoken(tokenstring)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": "无效的token",
+			})
+			c.Abort()
+		}
+		c.Next()
+	}
+}
--- route/route.go
+++ route/route.go
@@ -3,17 +3,18 @@ package route
 import (
 	"github.com/gin-gonic/gin"
 	ctrl "liuliang/controller"
+	mid "liuliang/midware"
 )
 
 func Init() *gin.Engine {
 	r := gin.Default()
 	usergroup := r.Group("/user")
 	usergroup.POST("/register", ctrl.Userregister)
-	usergroup.POST("/login", ctrl.Userlogin)
-	usergroup.POST("/updatapasswd", ctrl.Usernewpasswd)
-	usergroup.GET("/back_infor", ctrl.User_backinfor)
-	usergroup.POST("/del", ctrl.Userdel)
-	pptgroup := r.Group("/property")
+	usergroup.POST("/login", mid.Sendtoken(), ctrl.Userlogin)
+	usergroup.POST("/updatapasswd", mid.Twjparse(), ctrl.Usernewpasswd)
+	usergroup.GET("/back_infor", mid.Twjparse(), ctrl.User_backinfor)
+	usergroup.POST("/del", mid.Twjparse(), ctrl.Userdel)
+	pptgroup := r.Group("/property", mid.Twjparse())
 	pptgroup.POST("/upload", ctrl.Pptupload)
 	pptgroup.POST("/search", ctrl.Pptsearch)
 	pptgroup.POST("/updata", ctrl.Pptupdata)
